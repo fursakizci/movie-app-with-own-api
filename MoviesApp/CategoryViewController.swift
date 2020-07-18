ea880142a0cf2a9b6fc44304e2243c4383de69ee
furkan SAKIZCI <fursakizci@gmail.com>
2020-07-18 17:19:37 +0300

    create new project

---
+++ MoviesApp/CategoryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Furkan sakızcı on 18.07.2020.
+//  Copyright © 2020 Furkan sakızcı. All rights reserved.
+//
+
+import UIKit
+
+class CategoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
