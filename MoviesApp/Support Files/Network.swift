c2bb290905b4dd2560f9259e026a7c219ab22914
furkan SAKIZCI <fursakizci@gmail.com>
2020-07-24 11:52:03 +0300

    create network class

---
+++ MoviesApp/Support Files/Network.swift
@@ -0,0 +1,13 @@
+//
+//  Network.swift
+//  MoviesApp
+//
+//  Created by Furkan sakızcı on 24.07.2020.
+//  Copyright © 2020 Furkan sakızcı. All rights reserved.
+//
+
+import Foundation
+
+class Network{
+    
+}
